89082a058494716f2c8472c26fbc57e18cbe4408
NafisianCastle <nafis.cseaiub@gmail.com>
2022-04-08 15:29:04 +0600

    update nafis

---
+++ BLL/Entities/UserAnswerCommentModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BLL.Entities
+{
+    public class UserAnswerCommentModel
+    {
+        
+    }
+}
--- BLL/Services/VoteService.cs
+++ BLL/Services/VoteService.cs
@@ -9,38 +9,60 @@ namespace BLL.Services
 {
     public class VoteService
     {
-        public static VoteModel Get(int id)
+        public static List<VoteModel> GetAllVotes()
         {
-            var st = DataAccessFactory.VoteDataAccess().Get(id);
-            var s = new VoteModel()
+            var config = new MapperConfiguration(c =>
             {
-                VoteId = st.VoteId,
-                UserId = st.UserId,
-                Date = st.Date,
-                PostId = st.PostId,
-                Value = st.Value
-            };
-            return s;
-        }
-        public static List<VoteModel> Get()
-        {
-            var sts = DataAccessFactory.VoteDataAccess().Get();
-            return sts.Select(s => new VoteModel()
-            {
-                VoteId = s.VoteId,
-                UserId = s.UserId,
-                Date = s.Date,
-                PostId = s.PostId,
-                Value = s.Value
+                c.CreateMap<Vote, VoteModel>();
 
-            }).ToList();
-        }
-        public static List<VoteModel> GetAll()
-        {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<Vote, VoteModel>());
+            });
             var mapper = new Mapper(config);
-            var data = mapper.Map<List<VoteModel>>(DataAccessFactory.VoteDataAccess().Get());
+            var da = DataAccessFactory.VoteDataAccess();
+            var data = mapper.Map<List<VoteModel>>(da.Get());
             return data;
+        }
+        
+
+        public static VoteModel GetVote(int id)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Vote, VoteModel>();
+            });
+            var mapper = new Mapper(config);
+            var da = DataAccessFactory.VoteDataAccess();
+            var data = mapper.Map<VoteModel>(da.Get(id));
+            return data;
+        }
+
+        public static void AddVote(VoteModel u)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<VoteModel, Vote>();
+
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Vote>(u);
+            DataAccessFactory.VoteDataAccess().Add(data);
+        }
+
+        public static void EditVote(VoteModel u)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<VoteModel, Vote>();
+
+            });
+            var mapper = new Mapper(config);
+            var data = mapper.Map<Vote>(u);
+            DataAccessFactory.VoteDataAccess().Edit(data);
+
+        }
+
+        public static void DeleteVote(int id)
+        {
+            DataAccessFactory.VoteDataAccess().Delete(id);
         }
     }
 }
